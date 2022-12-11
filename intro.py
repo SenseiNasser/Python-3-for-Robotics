a4a5965839922e87f51342d3968d94ad25f431f3
SenseiNasser <65749356+SenseiNasser@users.noreply.github.com>
2022-12-11 03:14:24 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,11 @@
+
+# Python is the most popular programming language for robots
+# together with C++
+# Many of the artificial intelligence libraries used for robotics are written in Python (e.g. OpenCV, ROS, PyTorch, TensorFlow, etc.)
+# ROS (Robot Operating System) is a set of software libraries and tools that help you build robot applications
+# ROS is a framework for writing robot software.
+# ROS is a collection of tools, libraries and conventions that aim to simplify the task of creating complex and robust robot behavior across a wide variety of robotic platforms.
+# ROS language is Python and C++
+# you want to become a ROS developer, you need to learn Python and C++
+
+# This files is exersices for learning Python with ROS
